8ecbebddb41f93523682e9d1a1e14f3bb219b872
itsbasilsaman <itsbasilsaman@gmail.com>
2025-01-16 19:17:41 +0530

    fdsf

--- src/components/forms/user/phoneVerification.tsx
+++ src/components/forms/user/phoneVerification.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "react-phone-number-input/style.css"; // Add styles for the phone input
-import BgImg from "../../../assets/images/logo.png";
+// import BgImg from "../../../assets/images/logo.png";
 
 export const MainVerification = () => {
   const [otp, setOtp] = useState<string[]>(["", "", "", "", "", ""]);
@@ -75,7 +75,7 @@ export const MainVerification = () => {
         {/* Semi-transparent Overlay */}
         <div className="absolute inset-0 adminlogin-background">
           <div className="background-one relative inset-0 flex justify-center items-start pt-[60px]">
-            <img src={BgImg} alt="" className="w-[110px]" />
+            {/* <img src={BgImg} alt="" className="w-[110px]" /> */}
           </div>
           <div className="background-two bg-white"></div>
         </div>
