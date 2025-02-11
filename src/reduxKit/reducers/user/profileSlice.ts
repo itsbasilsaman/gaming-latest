a7cdff0e715cdeb1fb9251b5f3589a149154417d
itsbasilsaman <itsbasilsaman@gmail.com>
2025-02-11 11:42:03 +0530

    error fix

--- src/reduxKit/reducers/user/profileSlice.ts
+++ src/reduxKit/reducers/user/profileSlice.ts
@@ -1,50 +1,52 @@
+ 
+import { getUserProfile } from "../../actions/user/userProfile";
 
- import { getUserProfile } from "../../actions/user/userProfile";
-
-
-
+ 
+ 
 import { createSlice,  } from "@reduxjs/toolkit";
+ 
 
-export interface UserLanguageState {
+ 
+ 
+export interface ProfileLoadingStatus {
+  error: string | null;
+  GetProfileloading: boolean
+}
 
-    error: string | null;
-    GetProfileloading: boolean;
-  }
-  const initialStateForLanguage: UserLanguageState = {
-    error: null,
-    GetProfileloading: false,
-  };
-
+const initialStateForProfileLoading:ProfileLoadingStatus = {
+  error: null,
+  GetProfileloading: false,
+}
 
   
-export const ProfileSlice = createSlice({
-    name: "/userProfileSlice",
-    initialState: initialStateForLanguage,
-    reducers: {
-      updateError: (state, { payload }) => {
-        state.error = payload;
-      },
-    },
-  
+ 
 
-    extraReducers: (builder) => {
-      builder
-        .addCase(getUserProfile.pending, (state) => {
-          state.GetProfileloading = true;
-          state.error = null;
-        })
-        .addCase(getUserProfile.fulfilled, (state, { payload }) => {
-          console.log("Payload after language change:", payload);
-          state.GetProfileloading = false;
-          state.error = null;
-        
-       
-        })
-        .addCase(getUserProfile.rejected, (state, { payload }) => {
-          state.GetProfileloading = false;
-          state.error = payload as string;
-        });
-    },
-  });
-  
-  export default ProfileSlice.reducer;
+
+ export const ProfileSlice = createSlice({
+   name: '/userProfileSlice',
+   initialState: initialStateForProfileLoading,
+   reducers : {
+    updateError : (state, {payload})=> {
+      state.error = payload;
+    }
+   },
+   extraReducers : (builder) => {
+    builder 
+    .addCase(getUserProfile.pending, (state)=> {
+      state.GetProfileloading = true;
+      state.error = null;
+    })
+    .addCase(getUserProfile.fulfilled, (state , {payload}) => {
+      console.log("Payload after language change:", payload);
+      
+      state.GetProfileloading = false;
+      state.error = null;
+    })
+    .addCase(getUserProfile.rejected, (state , {payload}) => {
+      state.GetProfileloading= false;
+      state.error = payload as string;
+    });
+   },
+ })
+
+ export default ProfileSlice.reducer;
